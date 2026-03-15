df3e078924490284c3b703f20a902617443c37f6
Yuato <tonyyuan1010@gmail.com>
2026-03-15 18:46:26 -0400

    finished background

--- js/background.js
+++ js/background.js
@@ -8,7 +8,10 @@ window.addEventListener("load", function(){
         constructor(startX, startY, width, height, color, direction){
             this.end = false;
             this.turn = false;
-            this.startX = 
+            this.startX = startX;
+            this.startY = startY;
+            this.width = width;
+            this.height = height;
             this.posX = startX;
             this.posY = startY;
             this.color = color;
@@ -17,19 +20,17 @@ window.addEventListener("load", function(){
             this.turnX = startX;
             this.turnY = startY;
             if (direction%2 === 0){
-                this.travel = Math.floor(height*Math.random());
+                this.travel = 50+Math.floor((width-100)*Math.random());
                 this.turnY = this.travel;
             }   
             else{
-                this.travel = Math.floor(width*Math.random());
+                this.travel = 50+Math.floor((height-100)*Math.random());
                 this.turnX = this.travel;
             }
         }
         newdirection(check){
             if (check === this.travel && this.turn == false){
-                console.log("hello");
                 this.direction = (3 + this.direction + Math.floor(3*Math.random()))%4;
-                console.log(this.direction);
                 this.turn = true;
             }
         }
@@ -52,12 +53,19 @@ window.addEventListener("load", function(){
                     this.newdirection(this.posX);
                 }
             }
+            if (this.posX < 0 || this.posX > this.width){
+                this.end = true;
+            }
+            else if (this.posY < 0 || this.posY > this.height){
+                this.end = true;
+            }
         }
 
         draw(ctx) {
             if (this.turn){
-                ctx.fillSyle = this.color;
+                ctx.strokeStyle = this.color;
                 ctx.lineWidth = 10;
+                ctx.beginPath()
                 ctx.moveTo(this.startX, this.startY);
                 ctx.lineTo(this.turnX,this.turnY);
                 ctx.lineTo(this.posX, this.posY);
@@ -66,33 +74,79 @@ window.addEventListener("load", function(){
             else{
                 ctx.strokeStyle = this.color;
                 ctx.lineWidth = 10;
+                ctx.beginPath()
                 ctx.moveTo(this.startX, this.startY);
                 ctx.lineTo(this.posX, this.posY);
                 ctx.stroke();
             }
         }
+    }
 
+    function generateFlows(array){
+        let red = Math.floor(200*Math.random())+50;
+        let blue = Math.floor(200*Math.random())+50;
+        let green = Math.floor(200*Math.random())+50;
+        let color = "rgb("+red+","+blue+","+green+")";
+        let startX = 0;
+        let startY = 0;
+        let direction = Math.floor(Math.random()*4);
+        if (direction%2 === 0){
+            startX = 50+Math.floor((canvas.width-100)*Math.random());
+            if (direction == 0){
+                startY = canvas.height;
+            }
+        }   
+        else{
+            startY = 50+Math.floor((canvas.height-100)*Math.random());
+            if (direction == 3){
+                startX = canvas.width;
+            }
+        }
+        array.push(new Flow(startX,startY,canvas.width,canvas.height,color,direction));
+    }
+
+    function checkFlows(array){
+        let end = true;
+        for (let i = 0; i < array.length; i++){
+            end = (end && array[i].end);
+        }
+        return end;
     }
 
     const canvas = this.document.getElementById("backgroundCanvas");
     const ctx = canvas.getContext("2d");
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
 
     const flows = [];
 
-    let line = new Flow(20,0,canvas.width,canvas.height,"red",2);
+    
     function animate(){
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-        ctx.fillStyle = "black"; // Set the background color to lightblue, for example
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "black"; 
         ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-        ctx.strokeStyle = "red";
-        ctx.lineWidth = 10;
-        ctx.strokeRect(125,25,150,50);
 
-        line.update();
-        line.draw(ctx);
+        if (checkFlows(flows)){
+            flows.length = 0;
+            for (let i = 0; i < 8; i++){
+                generateFlows(flows);
+            }
+        }
+
+        for (let i = 0; i < flows.length; i++){
+            flows[i].update();
+            flows[i].draw(ctx);
+        }
+
         requestAnimationFrame(animate);
     }
+
+ 
+
+
+
+
 
     function createFlow(){
         
